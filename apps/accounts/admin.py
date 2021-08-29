c20e622822eda56f20152f0aed464b8cc2ba6a6b
Nagah <E-n.Shinawy@lean.sa>
2021-08-29 12:54:53 +0300

    custom user representation on admin panel-2

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
 
 admin.site.unregister(User)
 
 
 @admin.register(User)
-class UserModelAdmin(admin.ModelAdmin):
+class UserModelAdmin(UserAdmin):
     list_display = ("id", "username", "email", "is_superuser")
     search_fields = ("id", "username", "email")
     list_editable = ("username", )
