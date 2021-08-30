4871ff47b7f9f774324578d0dc4d46651db4200f
Nagah <E-n.Shinawy@lean.sa>
2021-08-30 07:55:47 +0300

    adding single profile

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def projects(request):
     return render(request, template_name="projects/projects.html")
+
+
+def project(request, uuid):
+    return render(request, template_name="projects/single-project.html")
+
