39edc9f48d523dc3b8d9ef6151506a5695caaea7
Nagah <E-n.Shinawy@lean.sa>
2021-08-30 07:32:10 +0300

    adding login, sign in, forgetpassword templates

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import profile
+from .views import profile, login, logout, signup, forgetpassword
 
 
 app_name = "accounts"
@@ -7,4 +7,8 @@ app_name = "accounts"
 
 urlpatterns = [
     path("", profile, name="profile"),
+    path("login/", login, name="login"),
+    path("signup/", signup, name="signup"),
+    path("logout/", logout, name="logout"),
+    path("forgetpassword/", forgetpassword, name="forgetpassword"),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def profile(request):
     return render(request=request, template_name="accounts/profile.html")
+
+
+def signup(request):
+    return render(request=request, template_name="accounts/signup.html")
+
+
+def login(request):
+    return render(request=request, template_name="accounts/login.html")
+
+
+def logout(request):
+    return HttpResponse("You are logged out")
+
+
+def forgetpassword(request):
+    return render(request=request, template_name="accounts/forgetpassword.html")
