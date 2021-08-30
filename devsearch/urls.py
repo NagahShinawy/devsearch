7d4712611406e77ef5b8b29d2b8c218e50180c61
Nagah <E-n.Shinawy@lean.sa>
2021-08-30 07:19:03 +0300

    adding account/profile template

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -25,7 +25,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("developers/", include("apps.developers.urls")),
     path("projects/", include("apps.projects.urls")),
-    path("inbox/", include("apps.contacts.urls"))
+    path("inbox/", include("apps.contacts.urls")),
+    path("accounts/", include("apps.accounts.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
