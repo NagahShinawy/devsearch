7b24a27487b4aff6c010b199f3c84cdfd52e6761
Nagah <E-n.Shinawy@lean.sa>
2021-08-31 20:24:48 +0300

    adding Tag, Review to admin

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Tag, Review
 
 
 @admin.register(Project)
@@ -8,7 +8,29 @@ class ProjectModelAdmin(admin.ModelAdmin):
 
     list_display = ("uuid", "title", "slug", "created", "updated")
     list_display_links = ("uuid", "title", "slug")
-    search_fields = ("uuid", "title", "description")
+    search_fields = ("uuid", "title", "description", "tags__name", "reviews__body")
     readonly_fields = ("uuid", "slug")
+
+    list_per_page = 10
+
+
+@admin.register(Review)
+class ReviewModelAdmin(admin.ModelAdmin):
+    date_hierarchy = "created"
+
+    list_display = ("uuid", "content", "value", "project", "created")
+    search_fields = ("uuid", "body", "value")
+    readonly_fields = ("uuid", "value")
+
+    list_per_page = 10
+
+
+@admin.register(Tag)
+class TagModelAdmin(admin.ModelAdmin):
+    date_hierarchy = "created"
+
+    list_display = ("uuid", "name", "created")
+    search_fields = ("uuid", "name")
+    readonly_fields = ("uuid",)
 
     list_per_page = 10
