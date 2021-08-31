2a4b67c188d1da20e936610e4350796a6add530a
Nagah <E-n.Shinawy@lean.sa>
2021-08-31 19:43:48 +0300

    issue of (models.E006)

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,5 +1,7 @@
+import uuid
 from django.db import models
 from apps.core.db.models import UUIDMixin, InfoMixin, SlugMixin, TimestampMixin
+from .choices import VoteType
 
 
 class Project(UUIDMixin, InfoMixin, SlugMixin, TimestampMixin):
@@ -10,3 +12,21 @@ class Project(UUIDMixin, InfoMixin, SlugMixin, TimestampMixin):
         ordering = ["-created"]
         verbose_name = "Project"
         verbose_name_plural = "Lean Projects"
+
+
+class Review(UUIDMixin, TimestampMixin, models.Model):
+    # owner
+
+    # on_delete ==> what you will do with children if parent deleted ?
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=256, choices=VoteType.choices)
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="reviews")
+
+    def __str__(self):
+        return self.value
+
+
+class Tag(UUIDMixin, TimestampMixin, models.Model):
+    name = models.CharField(max_length=256)
+    projects = models.ManyToManyField(Project, related_name="tags")
