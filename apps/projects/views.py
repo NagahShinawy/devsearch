2c688b8fdf69cbabe5177a28b2fa1d681f1ade6d
Nagah <E-n.Shinawy@lean.sa>
2021-09-03 11:10:54 +0300

    clean up / black

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -4,33 +4,12 @@ from django.shortcuts import render
 
 from apps.projects.models import Project
 
-sehhaty = {
-    "id": 1,
-    "name": "Sehhaty",
-    "users": 45345,
-    "developers": 20,
-}
-
-gcc = {
-    "id": 2,
-    "name": "gcc",
-    "users": 900,
-    "developers": 80,
-}
-
-anat = {
-    "id": 3,
-    "name": "Anat",
-    "users": 1010,
-    "developers": 120,
-}
-
-apps = [sehhaty, gcc, anat]
-
 
 def projects(request):
     projs = Project.objects.all()
-    return render(request, template_name="projects/projects.html", context={"projects": projs})
+    return render(
+        request, template_name="projects/projects.html", context={"projects": projs}
+    )
 
 
 def project(request, uuid):
