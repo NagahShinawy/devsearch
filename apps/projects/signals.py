558bd62e2653b141424cc00976fffe7c9643c5b4
Nagah <E-n.Shinawy@lean.sa>
2021-09-09 07:16:40 +0300

    delete project image from server after delete instance

--- apps/projects/signals.py
+++ apps/projects/signals.py
@@ -1,5 +1,6 @@
 import logging
-from django.db.models.signals import pre_save
+import os
+from django.db.models.signals import pre_save, post_delete
 from django.dispatch import receiver
 from django.utils.text import slugify
 from .models import Project
@@ -12,10 +13,19 @@ def get_project_representation(instance):
     return project
 
 
-@receiver(pre_save, sender=Project, dispatch_uid="update_slug_unlisted")
+@receiver(pre_save, sender=Project, dispatch_uid="update_slug")
 def update_slug(sender, instance, **kwargs):
     project = get_project_representation(instance)
     if instance.slug is not None:
         return
     instance.slug = slugify(instance.title)
     logger.info(f"Update 'slug' for {sender} <{project}> to <{instance.slug}>")
+
+
+@receiver(post_delete, sender=Project, dispatch_uid="delete_img")
+def delete_img(sender, instance, **kwargs):
+    project = get_project_representation(instance)
+    if not instance.image:
+        return
+    os.remove(instance.image.path)
+    logger.info(f"image <{instance.image}>  for {sender} <{project}> was deleted")
